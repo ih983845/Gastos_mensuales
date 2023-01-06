db1a35a9dfdd28264e43395d87695e3552260e83
nacho <ih983845@gmail.com>
2023-01-06 20:32:49 -0300

    TEST

---
+++ main.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+
+st.sidebar.title("Open the excel file")
+uploaded_file = st.sidebar.file_uploader("Choose a file",type= 'xlsx' )
+
+if uploaded_file is not None:
+    df = pd.read_excel(uploaded_file)
+
+    st.line_chart(data= df, x= "Fecha", y= "Amount")
+
+    st.bar_chart(data= df, x= "Tipo", y= "Amount")
+
+#test
